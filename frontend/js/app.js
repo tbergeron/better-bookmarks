dc4115adc7e975cff4c4ae877eb208283e80f657
Tommy Bergeron <tom@brainpad.org>
2016-03-02 18:55:23 -0500

    chrome extension
    - search
    - add/remote working

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,65 @@
-var firebaseRef = new Firebase('https://bbtestdb.firebaseio.com/');
+window.onload = function() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var currentUrl = tabs[0].url
+        var currentTitle = tabs[0].title
+        var baseURL = 'https://bbtestdb.firebaseio.com/'
+        var Bookmarks = new Firebase(baseURL + 'bookmarks')
+
+        Bookmarks.on('child_added', function(snapshot) {
+            var item = snapshot.val()
+            item.id = snapshot.key()
+            app.bookmarks.push(item)
+        })
+
+        Bookmarks.on('child_removed', function(snapshot) {
+            var id = snapshot.key()
+            app.bookmarks.some(function(bookmark) {
+                if (bookmark.id === id) {
+                    app.bookmarks.$remove(bookmark)
+                    return true
+                }
+            })
+        })
+        
+        var app = new Vue({
+            el: '#app',
+            searchQuery: '',
+            data: {
+                bookmarks: [],
+                newBookmark: {
+                    name: currentTitle,
+                    url: currentUrl
+                }
+            },
+            // computed property for form validation state
+            computed: {
+                validation: function() {
+                    return {
+                        name: !!this.newBookmark.name.trim(),
+                        url: !!this.newBookmark.url.trim(),
+                    }
+                },
+                isValid: function() {
+                    var validation = this.validation
+                    return Object.keys(validation).every(function(key) {
+                        return validation[key]
+                    })
+                }
+            },
+            // methods
+            methods: {
+                addBookmark: function() {
+                    if (this.isValid) {
+                        Bookmarks.push(this.newBookmark)
+                        this.newBookmark.name = ''
+                        this.newBookmark.url = ''
+                    }
+                },
+                removeBookmark: function(bookmark) {
+                    return Bookmarks.child(bookmark.id).remove()
+                }
+            }
+        }) // end of vue App
+
+    })
+}
