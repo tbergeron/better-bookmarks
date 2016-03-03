8804acbcaf1047c8c7f56872a2812b7f0265af32
Tommy Bergeron <tom@brainpad.org>
2016-03-02 23:20:11 -0500

    proof of concept complete
    - open first result press enter
    - edit bookmarks

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,44 +1,50 @@
-Vue.debug = true;
+Vue.debug = true
+var baseURL = 'https://bbtestdb.firebaseio.com/'
 
 function getRandomToken() {
     // E.g. 8 * 32 = 256 bits token
-    var randomPool = new Uint8Array(32);
-    crypto.getRandomValues(randomPool);
-    var hex = '';
+    var randomPool = new Uint8Array(32)
+    crypto.getRandomValues(randomPool)
+    var hex = ''
     for (var i = 0; i < randomPool.length; ++i) {
-        hex += randomPool[i].toString(16);
+        hex += randomPool[i].toString(16)
     }
     // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
-    return hex;
+    return hex
 }
 
 window.onload = function() {
-    document.getElementById('searchQuery').focus();
+    $('#searchQuery').focus()
 
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var currentUrl = tabs[0].url
         var currentTitle = tabs[0].title
 
         chrome.storage.sync.get('userid', function(items) {
-            var userid = items.userid;
+            var userid = items.userid
             if (userid) {
-                useToken(userid);
+                useToken(userid)
             } else {
-                userid = getRandomToken();
+                userid = getRandomToken()
                 chrome.storage.sync.set({userid: userid}, function() {
-                    useToken(userid);
-                });
+                    useToken(userid)
+                })
             }
             function useToken(userid) {
                 // logic starts here
-                var baseURL = 'https://bbtestdb.firebaseio.com/'
                 var Bookmarks = new Firebase(baseURL + userid + '/bookmarks')
-
                 Bookmarks.on('child_added', function(snapshot) {
                     var item = snapshot.val()
                     item.id = snapshot.key()
                     app.bookmarks.push(item)
                 })
+
+                // Bookmarks.on('child_changed', function(snapshot) {
+                //     console.log(snapshot);
+                //     var item = snapshot.val()
+                //     item.id = snapshot.key()
+                //     app.bookmarks.push(item)
+                // })
 
                 Bookmarks.on('child_removed', function(snapshot) {
                     var id = snapshot.key()
@@ -53,9 +59,10 @@ window.onload = function() {
                 var app = new Vue({
                     el: '#app',
                     searchQuery: '',
+                    currentObjectInfo: [],
                     data: {
                         bookmarks: [],
-                        newBookmark: {
+                        bookmarkForm: {
                             name: currentTitle,
                             url: currentUrl
                         }
@@ -64,8 +71,8 @@ window.onload = function() {
                     computed: {
                         validation: function() {
                             return {
-                                name: !!this.newBookmark.name.trim(),
-                                url: !!this.newBookmark.url.trim(),
+                                name: !!this.bookmarkForm.name.trim(),
+                                url: !!this.bookmarkForm.url.trim(),
                             }
                         },
                         isValid: function() {
@@ -77,12 +84,27 @@ window.onload = function() {
                     },
                     // methods
                     methods: {
-                        addBookmark: function() {
-                            if (this.isValid) {
-                                Bookmarks.push(this.newBookmark)
-                                this.newBookmark.name = ''
-                                this.newBookmark.url = ''
+                        editBookmark: function(bookmark) {
+                            this.bookmarkForm.name = bookmark.name
+                            this.bookmarkForm.url = bookmark.url
+                            this.currentObjectInfo = [app.bookmarks.indexOf(bookmark), bookmark.id]
+                            $('.edit-form').show()
+                            $('.edit-form .name').focus()
+                        },
+                        saveBookmark: function(id, bookmark) {
+                            var updatedBookmark = {
+                                name: this.bookmarkForm.name,
+                                url: this.bookmarkForm.url,
                             }
+                            // update vue collection
+                            app.bookmarks.$set(this.currentObjectInfo[0], updatedBookmark);
+                            // update firebase collection
+                            Bookmarks.child(this.currentObjectInfo[1]).set(updatedBookmark);
+                            // reset vars
+                            this.bookmarkForm.name = '';
+                            this.bookmarkForm.url = '';
+                            this.currentObjectInfo = [];
+                            $('.edit-form').hide();
                         },
                         removeBookmark: function(bookmark) {
                             return Bookmarks.child(bookmark.id).remove()
@@ -97,13 +119,13 @@ window.onload = function() {
                             chrome.tabs.create({ url: bookmark.url, active: true })
                         },
                         openFirstResult: function() {
-                            // todo: get first result from filter and open in new tab
-                            // this.openNewTab(app.bookmarks[0]);
+                            var url = $('ul li.bookmark:nth-child(1) span a').attr('title');
+                            this.openNewTab({ url: url })
                         }
                     }
                 }) // end of vue App
 
             } // end of useToken
-        }); // end of chrome.storage.sync
-    }); // end of chrome.tabs.query
+        }) // end of chrome.storage.sync
+    }) // end of chrome.tabs.query
 } // end of onload
