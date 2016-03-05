d4b4da0b90aac9a47651b166410e0429d97efe52
Tommy Bergeron <tom@brainpad.org>
2016-03-05 00:41:52 -0500

    some more lovin'

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,18 +7,16 @@ var baseURL = 'https://bbtestdb.firebaseio.com/'
 // todo: [x] keybinding to quickly open extension
 // todo: [x] star indicate if current page already bookmarked
 // todo: [x] delete confirmation / cancel timer popup
-// todo: [ ] tags support
+// todo: [x] tags support
 // todo: [ ] implement proper child_changed method
 
 function getRandomToken() {
-    // E.g. 8 * 32 = 256 bits token
     var randomPool = new Uint8Array(32)
     crypto.getRandomValues(randomPool)
     var hex = ''
     for (var i = 0; i < randomPool.length; ++i) {
         hex += randomPool[i].toString(16)
     }
-    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
     return hex
 }
 
@@ -49,7 +47,7 @@ window.onload = function() {
                 })
 
                 // Bookmarks.on('child_changed', function(snapshot) {
-                //     console.log(snapshot);
+                //     console.log(snapshot)
                 //     var item = snapshot.val()
                 //     item.id = snapshot.key()
                 //     app.bookmarks.push(item)
@@ -73,7 +71,8 @@ window.onload = function() {
                         bookmarks: [],
                         bookmarkForm: {
                             name: currentTitle,
-                            url: currentUrl
+                            url: currentUrl,
+                            tags: ''
                         }
                     },
                     // computed property for form validation state
@@ -86,7 +85,6 @@ window.onload = function() {
                         },
                         starClass: function() {
                             if (this.bookmarks) {
-                                // todo: OPTIMIZE THIS SHIT
                                 var result = ''
                                 this.bookmarks.forEach(function(bookmark) {
                                     if (bookmark.url == currentUrl) {
@@ -99,9 +97,17 @@ window.onload = function() {
                     },
                     // methods
                     methods: {
+                        showSuccessIndicator: function() {
+                            $('#success-indicator').fadeIn('slow', function() {
+                                setTimeout(function() {
+                                    $('#success-indicator').fadeOut('slow')
+                                }, 1000)
+                            })
+                        },
                         editBookmark: function(bookmark) {
                             this.bookmarkForm.name = bookmark.name
                             this.bookmarkForm.url = bookmark.url
+                            this.bookmarkForm.tags = bookmark.tags
                             this.currentObjectInfo = [app.bookmarks.indexOf(bookmark), bookmark.id]
                             $('.edit-form').show()
                             $('.edit-form .name').focus()
@@ -111,22 +117,27 @@ window.onload = function() {
                                 id: this.currentObjectInfo[1],
                                 name: this.bookmarkForm.name,
                                 url: this.bookmarkForm.url,
+                                tags: this.bookmarkForm.tags,
                             }
                             // update vue collection
                             app.bookmarks.$set(this.currentObjectInfo[0], updatedBookmark)
                             // update firebase collection
-                            var that = this;
+                            var that = this
                             Bookmarks.child(this.currentObjectInfo[1]).set(updatedBookmark, function() {
                                 // reset vars
                                 that.bookmarkForm.name = ''
                                 that.bookmarkForm.url = ''
+                                that.bookmarkForm.tags = ''
                                 that.currentObjectInfo = []
                                 $('.edit-form').hide()
+                                that.showSuccessIndicator()
                             })
                         },
                         removeBookmark: function(bookmark) {
-                            if (confirm('Are you sure to delete "' + bookmark.name + '"?'))
-                                return Bookmarks.child(bookmark.id).remove()
+                            if (confirm('Are you sure to delete "' + bookmark.name + '"?')) {
+                                Bookmarks.child(bookmark.id).remove()
+                                this.showSuccessIndicator()
+                            }
                         },
                         bookmarkCurrentPage: function() {
                             var already = false
@@ -142,16 +153,17 @@ window.onload = function() {
                             } else {
                                 Bookmarks.push({
                                     name: currentTitle,
-                                    url: currentUrl
+                                    url: currentUrl,
+                                    tags: ''
                                 })
                             }
-
+                            this.showSuccessIndicator()
                         },
                         openNewTab: function(bookmark) {
                             chrome.tabs.create({ url: bookmark.url, active: true })
                         },
                         openFirstResult: function() {
-                            var url = $('ul li.bookmark:nth-child(1) span a').attr('title');
+                            var url = $('ul li.bookmark:nth-child(1) span a').attr('title')
                             this.openNewTab({ url: url })
                         }
                     }
